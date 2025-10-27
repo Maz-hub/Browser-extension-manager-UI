53adcaf0e3893bfccb531de8f831f8ed511b5761
Maz <77918037+Maz-hub@users.noreply.github.com>
2025-10-27 15:29:23 +0000

    style(filter): fix tablet flex alignment and spacing for title and buttons

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,11 +1,11 @@
 const Filter = () => {
   return (
     <>
-      <section className="my-10">
-        <h1 className="text-preset-1 text-Neutral-900 dark:text-Neutral-0 text-center mb-6">
+      <section className="my-10 md:mb-8 md:flex md:justify-between md:items-center">
+        <h1 className="text-preset-1 text-Neutral-900 dark:text-Neutral-0 text-center md:text-left md: mb-6 md:mb-0">
           Extensions List
         </h1>
-        <div className="flex justify-around ">
+        <div className="flex justify-around md:gap-3 md:">
           <button className="btn btn-primary btn-sm">All</button>
           <button className="btn btn-outline btn-sm">Active</button>
           <button className="btn btn-outline btn-sm">Inactive</button>
